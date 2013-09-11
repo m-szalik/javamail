5535f25a0e671e191a835bd144e2e4d70379fb27
Marek Szalik <marek.szalik@jsoftware.org>
2013-09-11 10:23:34 +0000

    project module: test-webapp

---
+++ javamail-test-webapp/src/main/java/org/jsoftware/javamail/TestServlet.java
@@ -0,0 +1,52 @@
+package org.jsoftware.javamail;
+
+
+import javax.annotation.Resource;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class TestServlet extends HttpServlet {
+
+	@Resource(lookup = "mail/Session")
+	private transient Session mailSession;
+
+
+    @Override
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse response) throws ServletException, IOException {
+        httpServletRequest.getRequestDispatcher("/index.jsp").forward(httpServletRequest, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse response) throws ServletException, IOException {
+        MimeMessage message = new MimeMessage(mailSession);
+        String to = httpServletRequest.getParameter("to");
+        if (to == null || to.length() == 0) {
+            throw new ServletException("No \"to\" parameter!");
+        }
+        try {
+            Date now = new Date();
+            message.setFrom(new InternetAddress(mailSession.getProperty("mail.from")));
+            InternetAddress[] address = {new InternetAddress(to)};
+            message.setRecipients(Message.RecipientType.TO, address);
+            message.setSubject("JavaMail Test - " + now);
+            message.setSentDate(now);
+            message.setText("This is test message sent by org.jsoftware.javamail:javamail-test-webapp.");
+            Transport.send(message);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ServletException("Error sending mail!", ex);
+        }
+        doGet(httpServletRequest, response);
+    }
+
+
+}
---
+++ javamail-test-webapp/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<html>
+    <head>
+        <title>JavaMail test</title>
+    </head>
+
+    <body>
+        <form method="post">
+            Send email to: <input type="email" name="to"/>
+            <br/>
+            <input type="submit" value="send test message"/>
+        </form>
+    </body>
+
+</html>
