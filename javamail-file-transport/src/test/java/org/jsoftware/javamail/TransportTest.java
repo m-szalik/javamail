fa5cec0b2d1216830c756fdce7f833b9291ad493
Marek Szalik <szalik@jsoftware.org>
2014-03-12 00:11:09 +0100

    Better JUnit test

--- javamail-file-transport/src/test/java/org/jsoftware/javamail/TransportTest.java
+++ javamail-file-transport/src/test/java/org/jsoftware/javamail/TransportTest.java
@@ -3,15 +3,13 @@ package org.jsoftware.javamail;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.mail.Address;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.mail.*;
+import javax.mail.internet.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Properties;
 
@@ -36,12 +34,14 @@ public class TransportTest {
 	public void transportTxtTest() throws MessagingException, IOException, NoSuchAlgorithmException {
 		AbstractFileTransport transport = (AbstractFileTransport) session.getTransport("filetxt");
         transport.writeMessage(generateMessage(), toAddress, outputStream);
+        org.junit.Assert.assertEquals("4c7fb32ade5f1e860d9f8234b1ba8d5a", md5sumFromOutput());
 	}
 
     @Test
     public void transportMsgTest() throws MessagingException, IOException, NoSuchAlgorithmException {
         AbstractFileTransport transport = (AbstractFileTransport) session.getTransport("filemsg");
         transport.writeMessage(generateMessage(), toAddress, outputStream);
+        org.junit.Assert.assertEquals("789f256c33750a89b804a6c667e85d35", md5sumFromOutput());
     }
 
     @Test
@@ -51,13 +51,37 @@ public class TransportTest {
     }
 
 
+    /**
+     * Generate multi part / alternative message
+     */
     private MimeMessage generateMessage() throws MessagingException {
         MimeMessage msg = new MimeMessage(session);
         msg.setFrom("Test <from@test.com>");
-        msg.setText("Text");
         msg.setSubject("subject");
+        MimeBodyPart textPart = new MimeBodyPart();
+        textPart.setText("Body's text (text)", "UTF-8");
+        MimeBodyPart htmlPart = new MimeBodyPart();
+        htmlPart.setContent("<p>Body's text <strong>(html)</strong></p>", "text/html; charset=UTF-8");
+        Multipart multiPart = new MimeMultipart("alternative");
+        multiPart.addBodyPart(textPart); // first
+        multiPart.addBodyPart(htmlPart); // second
+        msg.setContent(multiPart);
         return msg;
     }
 
+
+    private String md5sumFromOutput() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        String content = new String(outputStream.toByteArray(), "UTF-8");
+        MessageDigest m = MessageDigest.getInstance("MD5");
+        m.reset();
+        for(String l : content.split("\n")) {
+            if (! l.startsWith("Date:") && ! l.startsWith("Message-ID:") && ! l.contains("Part")) {
+                m.update(l.getBytes());
+            }
+        }
+        byte[] digest = m.digest();
+        BigInteger bigInt = new BigInteger(1, digest);
+        return bigInt.toString(16);
+    }
 }
 
